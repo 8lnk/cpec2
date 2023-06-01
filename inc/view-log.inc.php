9bc252cf53c2eb44a2d3e712e3d5c8aa4e9f877c
8lnk <88780604+8lnk@users.noreply.github.com>
2023-06-01 15:59:44 +0300

    Update view-log.inc.php

--- inc/view-log.inc.php
+++ inc/view-log.inc.php
@@ -1,8 +1,13 @@
 <?php
 if(is_file('log/'.PATH_LOG)) {
   $logs = file('log/'.PATH_LOG);
+  echo "<ol>";
   foreach ($logs as $log) {
     $arrLog = explode('|', $log);
+    // list($dt, $page, $ref) = explode('|', $log);
+    echo "<li>";
     echo date('d-m-Y H-i-s', $arrLog[0]), ' - ', $arrLog[1], ' -> ', $arrLog[2], '<br>';
+    echo "</li>";
   }
+  echo "</ol>
 }
