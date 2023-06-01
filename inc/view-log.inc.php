2cddeccede6bb73df32f08a082049351114a4cb3
8lnk <88780604+8lnk@users.noreply.github.com>
2023-06-01 15:12:20 +0300

    вывел данные из path.log

--- inc/view-log.inc.php
+++ inc/view-log.inc.php
@@ -1,1 +1,8 @@
-<?
+<?php
+if(is_file('log/'.PATH_LOG)) {
+  $logs = file('log/'.PATH_LOG);
+  foreach ($logs as $log) {
+    $arrLog = explode('|', $log);
+    echo date('d-m-Y H-i-s', $arrLog[0]), ' - ', $arrLog[1], ' -> ', $arrLog[2], '<br>';
+  }
+}
