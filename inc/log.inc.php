38a4539090cc998f0220feb19a4dc6129178a747
8lnk <88780604+8lnk@users.noreply.github.com>
2023-06-01 15:37:00 +0300

    Update log.inc.php

--- inc/log.inc.php
+++ inc/log.inc.php
@@ -1,6 +1,8 @@
 <?php 
 $dt = time();
-$page = $_SERVER['REQUEST_URI'];
+//$page = $_SERVER['REQUEST_URI'];
+//$page = $_SERVER['QUERY_STRING'];
+$page = $_GET['id'];
 $ref = $_SERVER['HTTP_REFERER'};
 $path = $dt. '|'.$page.'|'.$ref.'<br>';
 file_put_contents('log/'.PATH_LOG, $path, FILE_APPEND);
