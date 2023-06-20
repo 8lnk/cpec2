7ae0c1a1e8cffe25513d22e65f7a3c25c00e96ba
8lnk <88780604+8lnk@users.noreply.github.com>
2023-06-20 12:13:33 +0300

    Update delete_from_basket.php

--- eshop/delete_from_basket.php
+++ eshop/delete_from_basket.php
@@ -2,3 +2,9 @@
 	// подключение библиотек
 	require "inc/lib.inc.php";
 	require "inc/config.inc.php";
+	if($_GET['id']) {
+		$id = clearInt($_GET['id']);
+		deleteFromBasket($id);
+	} 
+	header("Location: basket.php");
+	
